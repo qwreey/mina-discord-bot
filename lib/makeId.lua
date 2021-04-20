612a68fc2718ef9d9a08ad1af94d9c1c9d86a262
qwreey75 <qwreey75@gmail.com>
2021-04-20 21:21:25 +0900

    MINA : Sync in main code (Bot.lua)

--- lib/makeId.lua
+++ lib/makeId.lua
@@ -3,11 +3,12 @@ local WORD = {
 	"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 	"1","2","3","4","5","6","7","8","9","0"
 };
+local makeSeed = require "src/lib/makeSeed";
 return function ()
 	local ID = "";
 	for i = 1,18 do
-        math.randomseed(math.floor(((os.clock()+os.time())*1111) + ((i^2)*math.pi*10000)));
+        math.randomseed(makeSeed(math.pi^i,math.pi*i));
 		ID = ID .. WORD[math.random(1,#WORD)];
 	end
 	return ID;
-end
+end;
