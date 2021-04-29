138073214433fa605cd95c336e2089c3d14d8a88
qwreey75 <qwreey75@gmail.com>
2021-04-30 01:44:08 +0900

    PUSH

--- lib/Qeact/importElement.lua
+++
@@ -1,9 +0,0 @@
-return function(className)
-    return function (items)
-        local items = items or {};
-
-        items.IsElement = true;
-        items.ClassName = className;
-        return items;
-    end;
-end;
