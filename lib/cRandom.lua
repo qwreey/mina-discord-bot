52461787efd84fb0ee3c383c1966c77a70994b7a
qwreey75 <qwreey75@gmail.com>
2021-05-07 21:25:21 +0900

    test

--- lib/cRandom.lua
+++ lib/cRandom.lua
@@ -22,7 +22,7 @@ seed 를 루아 기본 난수 생성기에 집어넣고 루아 기본 난수 생
 
 ]]
 
-local pi3 = math.pi^13/10000000
+local pi3 = math.pi^13/10000000;
 return function (min,max)
 	local rm = (collectgarbage("count")%1*pi3)^2 * 1000000;
 	local ts = (os.clock()*pi3)^2;
--- lib/makeSeed.lua
+++ lib/makeSeed.lua
@@ -1,6 +1,6 @@
 local pi3 = math.pi^13/10000000;
 return function (min,max)
-	local rm = (collectgarbage("count")%1*pi3)^2 * 1000000;
+	local rm = (collectgarbage("count")*pi3)^2 * 1000000;
 	local ts = (os.clock()*pi3)^2;
 	local seed = math.floor(ts*((((min/13)^2+(max/11)^2)*pi3)^2+rm));
 
