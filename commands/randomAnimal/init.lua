76633c1643866fdac0e104034d06e5c5d2c5377f
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2021-09-22 20:14:03 +0900

    모든 띄어쓰기 인덴팅을 탭으로 변경

--- commands/randomAnimal/init.lua
+++ commands/randomAnimal/init.lua
@@ -1,5 +1,5 @@
 -- array = {
---     "https://cdn2.thecatapi.com/images/e4g.gif";
+--	 "https://cdn2.thecatapi.com/images/e4g.gif";
 
 
 -- }
@@ -10,11 +10,11 @@ request:setCoroHttp(corohttp):setJson(json);
 request:setUrlCode(urlCode);
 
 return {
-    ["동물"] = {
-        alias = {"동물사진","동물 사진","동물 가져오기","동물 사진 가져오기","렌덤 동물","렌덤동물","동물사진가져오기","동물가져오기"};
-        reply = "잠시만 기달려주세요... (불러오는중)";
-        func = function(replyMsg,message,args,Content)
-            replyMsg:setContent(request.fetch());
-        end;
-    };
+	["동물"] = {
+		alias = {"동물사진","동물 사진","동물 가져오기","동물 사진 가져오기","렌덤 동물","렌덤동물","동물사진가져오기","동물가져오기"};
+		reply = "잠시만 기달려주세요... (불러오는중)";
+		func = function(replyMsg,message,args,Content)
+			replyMsg:setContent(request.fetch());
+		end;
+	};
 };
