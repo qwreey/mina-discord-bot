55bc430f6a205054ded2bc88550c443399c0ddcd
살려주세요 <46598063+qwreey75@users.noreply.github.com>
2021-10-03 14:51:02 +0900

    cleanup

--- randomTest.lua
+++ randomTest.lua
@@ -1,8 +1,8 @@
-local min,max = 1,9;
+local min,max = 1,12;
 local cRandom = require("libs.cRandom");
 local drawPer = require("libs.drawPer");
 local tests = 100000;
-local titles = {"​슈​가​냥​이​가 ​젤​귀​엽​다","​존​경​이​가 ​젤​귀​엽​다","​냥​냥​이​가 ​젤​귀​엽​다","​댭​댜​비​가 ​젤​귀​엽​다","​익​명​이​가 ​젤​귀​엽​다","​콤​콥​이​가 ​잴​귀​엽​다","​몽​리​니​가 ​잴​귀​엽​다","​크​림​이​가 ​젤​귀​엽​다","​샴​플​이​가 ​젤​귀​엽​다","​샴​샴​이​가 ​젤​귀​엽​다"};
+-------------------------------------------------------------------------------------------------------------------------------------------------------local titles = {"​슈​가​냥​이​가 ​젤​귀​엽​다","​존​경​이​가 ​젤​귀​엽​다","​냥​냥​이​가 ​젤​귀​엽​다","​댭​댜​비​가 ​젤​귀​엽​다","​익​명​이​가 ​젤​귀​엽​다","​콤​콥​이​가 ​잴​귀​엽​다","​몽​리​니​가 ​잴​귀​엽​다","​크​림​이​가 ​젤​귀​엽​다","​샴​플​이​가 ​젤​귀​엽​다","​샴​샴​이​가 ​젤​귀​엽​다"};
 local uv = require("uv");
 local circles = {"◜","◝","◞","◟"};
 local circlesLen = #circles;
@@ -22,5 +22,6 @@ for i = 1,tests do
     array[index] = array[index] + 1;
 end
 for index,value in ipairs(array) do
-    drawPer.drawPerbarWithFrame(titles[index],value/tests,72);
+    --drawPer.drawPerbarWithFrame(titles[index],value/tests,72);
+    drawPer.drawPerbarWithFrame(tostring(index),value/tests,72);
 end
