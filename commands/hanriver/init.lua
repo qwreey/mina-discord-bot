8c3713da1b70ebb022c0d2103570fc5913d62ed3
살려주세요 <46598063+qwreey75@users.noreply.github.com>
2021-10-14 19:32:47 +0900

    bugfix

--- commands/hanriver/init.lua
+++ commands/hanriver/init.lua
@@ -4,7 +4,7 @@ return {
 	["한강"] = {
 		alias = {"한강물온도","한강 물 온도","한강 물온도","한강 각","한강각"};
 		reply = function ()
-            local response = corohttp.request("GET",APIurl);
+            local header,response = corohttp.request("GET",APIurl);
             local decoded = json.decode(response);
             if decoded then
                 return ("한강 물 온도는 %s 도 입니다!\n> %s 기준의 자료입니다"):format(response.temp,response.time);
