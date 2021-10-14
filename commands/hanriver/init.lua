df340c906744236d179cbbd78852a4d7148b9fae
살려주세요 <46598063+qwreey75@users.noreply.github.com>
2021-10-14 19:29:13 +0900

    친구땜 재미삼아 한강물온도 명령어 만듬

---
+++ commands/hanriver/init.lua
@@ -0,0 +1,15 @@
+local APIurl = "http://hangang.dkserver.wo.tc";
+
+return {
+	["한강"] = {
+		alias = {"한강물온도","한강 물 온도","한강 물온도","한강 각","한강각"};
+		reply = function ()
+            local response = corohttp.request("GET",APIurl);
+            local decoded = json.decode(response);
+            if decoded then
+                return ("한강 물 온도는 %s 도 입니다!\n> %s 기준의 자료입니다"):format(response.temp,response.time);
+            else return "한강 물 온도를 불러오지 못했습니다!";
+            end
+        end;
+	};
+};
