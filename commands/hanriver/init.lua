11061e1bd6d113b8e63096170a74b7da863ce07f
살려주세요 <46598063+qwreey75@users.noreply.github.com>
2021-10-14 19:33:14 +0900

    nil safe

--- commands/hanriver/init.lua
+++ commands/hanriver/init.lua
@@ -7,7 +7,7 @@ return {
             local header,response = corohttp.request("GET",APIurl);
             local decoded = json.decode(response);
             if decoded then
-                return ("한강 물 온도는 %s 도 입니다!\n> %s 기준의 자료입니다"):format(response.temp,response.time);
+                return ("한강 물 온도는 %s 도 입니다!\n> %s 기준의 자료입니다"):format(tostring(decoded.temp),tostring(decoded.time));
             else return "한강 물 온도를 불러오지 못했습니다!";
             end
         end;
