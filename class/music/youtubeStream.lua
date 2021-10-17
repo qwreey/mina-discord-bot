70180b18aac08e526cd76d3aec7f005a5a512be4
살려주세요 <46598063+qwreey75@users.noreply.github.com>
2021-10-17 21:08:42 +0900

    안정화 코드 적용

--- class/music/youtubeStream.lua
+++ class/music/youtubeStream.lua
@@ -15,30 +15,21 @@ local function download(url)
 		cwd = "./";
 		stdio = {nil,true,true};
 	});
-	local audio;
-	local info = "";
-	local traceback = "";
-	local index = 1;
+	local stdout = "";
 	for str in newProcess.stdout.read do
-		if index == 2 then
-			audio = str:gsub("\n","");
-		elseif index >= 3 then
-			info = info .. str;
-		end
-		traceback = traceback .. str;
-		index = index + 1;
+		stdout = stdout .. str;
 	end
-	newProcess.waitExit(); -- ah... it ok? idk. i just think, it should be on top of stdout.read
-	return audio, info, traceback, newProcess;
+	local splitted = strSplit(stdout,"\n");
+	-- newProcess.waitExit(); -- ah... it ok? idk. i just think, it should be on top of stdout.read
+	return splitted[2], splitted[3], stdout, newProcess;
 end
 
 function module.download(vid)
 	vid = module.getVID(vid);
-	local url = ('https://www.youtube.com/watch?v=%s'):format(vid);
-
 	if not vid then
 		error("You inputed invalid video id!");
 	end
+	local url = ('https://www.youtube.com/watch?v=%s'):format(vid);
 
 	-- if not exist already, create new it
 	local audio,info,traceback,newProcess = download(url);
@@ -47,7 +38,7 @@ function module.download(vid)
 	end
 
 	-- video was not found from youtube? or something want wrongly
-	local errormsg = ("something want wrong! video was not found from youtube or youtube-dl process was terminated with exit!\n```log\n%s\n```"):format(traceback);
+	local errormsg = ("something want wrong! video was not found from youtube or youtube-dl process was terminated with exit!");
 	logger.error(errormsg);
 	qDebug {
 		title = "failed to download video from youtube";
@@ -64,14 +55,16 @@ local vidFormat = ("[%w%-_]"):rep(11);
 local vidWatch = ("watch%%?v=(%s)"):format(vidFormat);
 local vidShort = ("https://youtu%%.be/(%s)"):format(vidFormat);
 local searchURLTemp = ("https://www.googleapis.com/youtube/v3/search?key=%s&part=snippet&maxResults=8&q=%%s"):format(ACCOUNTData.GoogleAPIKey);
-function module.search()
-	local Header,Body = corohttp.request("GET",
-		searchURLTemp:format(urlCode.urlEncode(Keyword))
+function module.search(url)
+	local _,Body = corohttp.request("GET",
+		searchURLTemp:format(urlCode.urlEncode(url))
 	);
 	if not Body then return end
 	Body = json.decode(Body);
 	if not Body then return end
-	local thing = Body[1];
+	local items = Body.items;
+	if not items then return end
+	local thing = items[1];
 	if not thing then return end
 	local id = thing.id;
 	if not id then return end
--- test/indexingCommand.lua
+++ test/indexingCommand.lua
@@ -1,4 +1,4 @@
-
+---@diagnostic disable
 function findCommand(...)
     p(...);
 end
