3558e5527ff0c8b3e7f127ae69e346ab3ace7132
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2021-11-19 14:54:47 +0900

    testing

--- app/data.lua
+++ app/data.lua
@@ -1,1 +1,17 @@
--- print("Data server")
+local corohttp = require 'coro-http';
+
+corohttp.createServer("http://127.0.0.1",25363,function (req, body)
+    print(req.method,req.params.name);
+    local payload = "testing";
+
+    local header = {
+        {"Content-Type","application/json"};
+        {"Content-Length",tostring(#payload)};
+        {"Connection", "close"};
+    };
+
+    header.code = 200;
+    header.reason = "OK";
+
+    return header,payload;
+end);
