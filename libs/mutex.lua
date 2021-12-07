2398f54ae4bf47ac46c5a4f7c74b031e757bf8f4
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2021-12-07 13:32:11 +0900

    mutex added

---
+++ libs/mutex.lua
@@ -0,0 +1,50 @@
+local mutex = {};
+mutex.__index = mutex;
+
+local insert = table.insert;
+local remove = table.remove;
+local resume = coroutine.resume;
+local running = coroutine.running;
+local yield = coroutine.yield;
+local wrap = coroutine.wrap;
+
+function mutex:lock(this)
+    local this = this or running();
+    if not this then
+        error("mutex:lock() must be runned on coroutine!");
+    end
+
+    if self.__locked then
+        local wait = self.__wait;
+        if not wait then
+            wait = {};
+            self.__wait = wait;
+        end
+        insert(wait,this);
+        return yield(mutex);
+    end
+
+    self.__locked = true;
+end
+
+function mutex:unlock()
+    local wait = self.__wait;
+    if wait and #wait ~= 0 then
+        local this = remove(wait,1);
+        wrap(resume)(this);
+        return mutex;
+    end
+    self.__locked = false;
+end
+
+function mutex:isLocked()
+    return self.__locked;
+end
+
+function mutex.new()
+    local new = {};
+    setmetatable(new,mutex);
+    return new;
+end
+
+return mutex;
