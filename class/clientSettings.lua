9b88821ad535adc37ccd78eb6b08cc3800dc9047
qwreey74 <qwreey75@gmail.com>
2021-12-12 17:38:33 +0900

    option fix

--- class/clientSettings.lua
+++ class/clientSettings.lua
@@ -2,15 +2,13 @@
 local floor = math.floor;
 _G.clientSettings = {
 	routeDelay = floor(1000/50);
-	largeThreshold = 4000;--2048;
+	largeThreshold = 2048;
 	cacheAllMembers = false;
 	compress = true;
-	bitrate = 64000; -- 72000
+	bitrate = 64000;
 	logFile = nil;
 	wssProps = {
-		['$os'] = "iOS",
 		['$browser'] = "Discord iOS";
-		['$device'] = "Discord iOS";
 	}
 };
 return _G.clientSettings;
