69ae4b2abece811c2d27c860aedcecfe4f6421cb
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2021-12-18 14:20:18 +0900

    Fixed reconnect delay max

--- deps/discordia_enchent/libs/containers/voice/FFmpegProcess.lua
+++ deps/discordia_enchent/libs/containers/voice/FFmpegProcess.lua
@@ -18,7 +18,7 @@ return function (FFmpegProcess)
 			'-reconnect', '1',
 			'-reconnect_at_eof', '1',
 			'-reconnect_streamed', '1',
-			'-reconnect_delay_max', '1024',
+			'-reconnect_delay_max', '5',
 			-- ss flag
 			'-vn', '-dn', '-sn', '-f', 's16le', 'pipe:1',
 			'-loglevel', 'warning', '-ignore_unknown', '-copy_unknown'
