458409a500011e83d8cba3203aaf462e9fb211d9
qwreey74 <qwreey75@gmail.com>
2022-01-02 18:34:07 +0900

    *ux support

--- app/path.lua
+++ app/path.lua
@@ -1,21 +1,26 @@
+local jit = require "jit";
 return function (path)
+    local jitos = jit.os;
 	local function add(newPath)
 		if string.find(path,newPath .. ";",0,true) then
 			return;
-		end
+        end
+        if jitos == "Windows" then
+            newPath = newPath:gsub("/","\\");
+        end
 		path = path .. newPath .. ";";
 	end
 
-	add(".\\?.lua");
-	add(".\\?\\init.lua");
-	add(".\\libs\\?.lua");
-	add(".\\libs\\?\\init.lua");
-	add(".\\deps\\?.lua");
-	add(".\\deps\\?\\init.lua");
-	add(".\\app\\?.lua");
-	add(".\\app\\?\\init.lua");
-	add(".\\bin\\?.lua");
-	add(".\\bin\\?\\init.lua");
-
+    local jitarch = jit.arch
+   	add("./?.lua");
+   	add("./?/init.lua");
+   	add("./libs/?.lua");
+   	add("./libs/?/init.lua");
+   	add("./deps/?.lua");
+   	add("./deps/?/init.lua");
+   	add("./app/?.lua");
+   	add("./app/?/init.lua");
+	add(("./bin/%s_%s"):format(jitos,jitarch) .. "/?.lua");
+	add(("./bin/%s_%s"):format(jitos,jitarch) .. "/?/init.lua");
 	return path;
 end;
