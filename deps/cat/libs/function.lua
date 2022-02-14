72306daae5c1ff2abb27cd7a11e5bb1b7f5df7ad
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2022-02-14 22:59:15 +0900

    asdf

--- deps/cat/libs/function.lua
+++ deps/cat/libs/function.lua
@@ -5,13 +5,13 @@ local gsub = string.gsub;
 local format = string.format;
 
 local function normalFormatter(str,method)
-    if method then return format(" = function(%s)",str); end
+    if method and method ~= "" then return format(" = function(%s)",str); end
     return format("function(%s)",str);
 end
 
 local function selfFormatter(str,method)
     local comma = str == "" and "" or ",";
-    if method then return format(" = function(self%s%s)",comma,str); end
+    if method and method ~= "" then return format(" = function(self%s%s)",comma,str); end
     return format("function(self%s%s)",comma,str);
 end
 
