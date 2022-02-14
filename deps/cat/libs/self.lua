fabb4a1ea61666e320b9c1691fe9777274f5f6b0
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2022-02-14 23:37:10 +0900

    asdf

--- deps/cat/libs/self.lua
+++ deps/cat/libs/self.lua
@@ -3,14 +3,14 @@ local module = {};
 local gsub = string.gsub;
 
 local function selfFormatter(all,st,mid,ed)
-    if st then
+    if st == "[" then
         return "self"..all;
     end
     return "self."..all;
 end
 
 function module.self(str)
-    return gsub(str,"@((%[?)([^ ]-)(%]?)?)",selfFormatter);
+    return gsub(str,"@((%[?)([^ ]-)(%]?))",selfFormatter);
 end
 
 return module;
