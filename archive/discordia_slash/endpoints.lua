1022792e05b571307e21ff9724ccbfa2922e6e15
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2022-03-01 18:15:53 +0900

    removed unused codes

--- archive/discordia_slash/endpoints.lua
+++
@@ -1,8 +0,0 @@
-return {
-	COMMANDS = "/applications/%s/commands",
-	COMMANDS_GUILD = "/applications/%s/guilds/%s/commands",
-	COMMANDS_MODIFY = "/applications/%s/commands/%s",
-	COMMANDS_MODIFY_GUILD = "/applications/%s/guilds/%s/commands/%s",
-	COMMAND_PERMISSIONS = "/applications/%s/guilds/%s/commands/permissions",
-	COMMAND_PERMISSIONS_MODIFY = "/applications/%s/guilds/%s/commands/%s/permissions"
-}
--- archive/discordia_voicefix/init.lua
+++
@@ -1,8 +0,0 @@
-local discordia = require("discordia");
-local classes = discordia.class.classes;
-require("FFmpegProcess")(classes.FFmpegProcess)
-require("VoiceConnection")(
-    classes.VoiceConnection,{
-        FFmpegProcess = classes.FFmpegProcess;
-    }
-);
