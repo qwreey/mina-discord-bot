c7ec8f0ec02c76b6b3a9b4c19ca2507eaba07011
qwreey75 <qwreey75@gmail.com>
2022-04-06 14:44:34 +0900

    사용자 지정 타자연습 추가됨

--- commands/typingGame/init.lua
+++ commands/typingGame/init.lua
@@ -20,7 +20,18 @@ local export = {
 	};
 	["타자연습"] = {
 		alias = "타자";
-		reply = "이용 가능한 타자 연습 게임은 '영문','한글' 입니다";
+		help = "이용 가능한 타자 연습 게임은 '영문','한글' 입니다\n> 또는 이 명령어 뒤에 원하는 글을 직접 입력할 수도 있어요";
+		waitting = {
+			content = "잠시만 기달려주세요 . . .";
+			embed = {description = "뒤에 보이는 문구를 재빠르게 입력하세요!"};
+		};
+		reply = function (message,args,Content,self)
+			local rawArgs = Content.rawArgs:gsub("^[\n \t]+",""):gsub("[\n \t]+$","");
+			if rawArgs == "" then
+				return self.help;
+			end
+			typingGame.new(message:reply(self.waitting),message,Content,rawArgs,"사용자 지정");
+		end;
 	};
 }
 return export;
