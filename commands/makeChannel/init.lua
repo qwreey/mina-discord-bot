442c74cad318cd6b08d0139b363f007034a699d7
Qwreey <qwreey75@gmail.com>
2022-05-29 17:55:26 +0900

    push

--- commands/makeChannel/init.lua
+++ commands/makeChannel/init.lua
@@ -65,6 +65,7 @@ client:onSync("voiceChannelJoin",promise.async(function (member, channel)
     end
 
     -- make new channel
+    if member.user.bot then return; end
     local this = guild:createVoiceChannel(channelData(channel,member));
     if not this then return logger.errorf("[ChannelMaker] Couldn't make channel in guild %s, ignore channelMaker function",guild.id); end -- permission missing? idk what happened...
     member:setVoiceChannel(this);
